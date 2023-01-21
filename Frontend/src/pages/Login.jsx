764321b96288b17eb8fd3916f3c628b0df739bec
vbhosale11 <vaibhavibhosale711@gmail.com>
2023-01-21 19:45:49 +0530

    pages added

---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+
+const LoginPage = () => {
+
+  return (
+    <div>
+        <Navbar />
+        <h1>Login</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
+
+
---
+++ Frontend/src/pages/homePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import  Navbar  from "../components/Navbar";
+
+export default function homePage(){
+    return (
+        <div>
+            <Navbar/>
+            <h1>Homepage</h1>
+        </div>
+    );
+}
