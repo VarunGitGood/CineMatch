35af017b194bed098079884441874384be4a1983
Prajjwal Kapoor <prajjwal.kapoor.mail@gmail.com>
2023-01-22 14:20:27 +0530

    some fix

--- Backend/middleware/protect.js
+++ Backend/middleware/protect.js
@@ -1,4 +1,4 @@
-const User = require("../model/User")
+const User = require("../model/User");
 const jwt = require("jsonwebtoken");
 const ErrorResponse = require("./errorResponse");
 const asyncHandler = require("./asyncHandler");
@@ -17,7 +17,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id);
     next();
   } catch (err) {
--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -4,11 +4,12 @@ import { Navigate } from "react-router-dom";
 import { AuthProvider } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-  const { token } = useContext(AuthProvider);
-  const [isValidUser, setIsValidUser] = useState(false);
+  const { token, isAuthenticated, setIsAuthenticated } =
+    useContext(AuthProvider);
   const validateUser = async () => {
     try {
       if (token) {
+        console.log("token is ", token);
         const response = await axios.get(
           "http://localhost:8000/api/v1/validate",
           {
@@ -19,25 +20,22 @@ const ProtectedRoute = ({ children }) => {
         );
         console.log("validateUser");
         console.log(response);
-        if (response.status === 200) {
-          setIsValidUser(true);
-        }
+        setIsAuthenticated(true);
       }
     } catch (error) {
       console.log(error);
-      setIsValidUser(false);
+      setIsAuthenticated(false);
     }
   };
 
   if (token) {
     validateUser();
-    if (isValidUser) {
+    if (isAuthenticated) {
       return children;
     } else {
       <Navigate to='/login' />;
     }
   }
-  return <Navigate to='/login' />;
 };
 
 export default ProtectedRoute;
